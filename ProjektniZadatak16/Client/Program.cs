1b621339edc1f408f205fd9b044d818fffe4d536
Nikola Petric <nikola2000petric@gmail.com>
2023-01-10 16:40:58 +0100

    Projekat

---
+++ ProjektniZadatak16/Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client");
+        }
+            
+    }
+}
