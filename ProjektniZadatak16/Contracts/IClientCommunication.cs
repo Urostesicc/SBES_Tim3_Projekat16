a1ab993e7b38bc8127dd7ee5db3667186fe70ee6
Nikola Petric <nikola2000petric@gmail.com>
2023-01-11 20:17:45 +0100

    InterfaceCC

---
+++ ProjektniZadatak16/Contracts/IClientCommunication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    [ServiceContract]
+    public interface IClientCommunication
+    {
+        [OperationContract]
+        void Connect();
+        [OperationContract]
+        void Disconnect();
+        [OperationContract]
+        void CreateConfigurationFile();
+        [OperationContract]
+        void DeleteConfigurationFile();
+        [OperationContract]
+        void AddEntry(ConfigurationEntry entry);
+        [OperationContract]
+        void ModifyEntry(ConfigurationEntry entry);
+        [OperationContract]
+        void DeleteEntry(int id);
+        [OperationContract]
+        List<ConfigurationEntry> ReadConfigurationFile();
+    }
+}
