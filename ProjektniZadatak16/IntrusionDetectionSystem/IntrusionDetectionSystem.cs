d9026e89de8ac9a0dc3ebe5199ac2a9945de3b38
Nikola Petric <nikola2000petric@gmail.com>
2023-01-13 19:50:58 +0100

    IDS.cs

---
+++ ProjektniZadatak16/IntrusionDetectionSystem/IntrusionDetectionSystem.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrusionDetectionSystem
+{
+    public class IntrusionDetectionSystem : IIntrusionDetectionSystem
+    {
+        public void UpdateIDS(Alarm alarm)
+        {
+            Console.WriteLine(alarm.ProcessName);
+            Console.WriteLine(alarm.CriticalityLevel);
+            Console.WriteLine(alarm.Timestamp);
+            Console.WriteLine();
+        }
+    }
+}
