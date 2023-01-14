4cd81bd1e754403728ca98466ab6dea43093ff19
Urostesicc <tesic.pr89.2019@uns.ac.rs>
2023-01-14 13:43:25 +0100

    klijent program

--- ProjektniZadatak16/Client/Program.cs
+++ ProjektniZadatak16/Client/Program.cs
@@ -12,8 +12,110 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Client");
+            //string name = "chiefOfficer";
+            string name = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            Console.WriteLine(name);
+
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/Service";
+
+            //Windows Authentification
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            string str;
+            bool exit = false;
+
+            using (Client proxy = new Client(binding, new EndpointAddress(new Uri(address))))
+            {
+                proxy.Connect();
+                while (!exit)
+                {
+                    Console.WriteLine("0 - Create Whitelist configuration file.");
+                    Console.WriteLine("1 - Delete Whitelist configuration file.");
+                    Console.WriteLine("2 - Add new entry to the Whitelist.");
+                    Console.WriteLine("3 - Modify existing entry.");
+                    Console.WriteLine("4 - Delete entry.");
+                    Console.WriteLine("5 - Read Whitelist configuration file.");
+                    Console.WriteLine("X - Shut Down.");
+
+                    str = Console.ReadLine();
+                    ConfigurationEntry newEntry;
+                    int id;
+
+                    switch (str)
+                    {
+                        case "0":
+                            proxy.CreateConfigurationFile();
+                            break;
+                        case "1":
+                            proxy.DeleteConfigurationFile();
+                            break;
+                        case "2":
+                            newEntry = CreateEntry();
+                            proxy.AddEntry(newEntry);
+                            break;
+                        case "3":
+                            Console.Write("Enter id of the entry you wish to modify: ");
+                            id = int.Parse(Console.ReadLine());
+                            newEntry = CreateEntry();
+                            newEntry.Id = id;
+                            proxy.ModifyEntry(newEntry);
+                            break;
+                        case "4":
+                            Console.Write("Enter id of the entry to delete: ");
+                            id = int.Parse(Console.ReadLine());
+                            proxy.DeleteEntry(id);
+                            break;
+                        case "5":
+                            List<ConfigurationEntry> entryList = proxy.ReadConfigurationFile();
+                            PrintWhitelist(entryList);
+                            break;
+                        case "X":
+                            exit = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Console.ReadLine();
+                proxy.Disconnect();
+            }
         }
+
+        private static ConfigurationEntry CreateEntry()
+        {
+            Console.Write("Enter name of the process: ");
+            string processName = Console.ReadLine();
+            Console.WriteLine("Enter Users that can start the process (X to finish): ");
+            List<string> users = new List<string>();
+            string user;
+            while (true)
+            {
+                user = Console.ReadLine();
+                if (user.Equals("X"))
+                    break;
+                users.Add(user);
+            }
+            return new ConfigurationEntry(1, processName, users);
+        }
+
+        private static void PrintWhitelist(List<ConfigurationEntry> entryList)
+        {
+            foreach (ConfigurationEntry entry in entryList)
+            {
+                Console.WriteLine("ID: " + entry.Id);
+                Console.WriteLine("Process Name: " + entry.ProcessName);
+                Console.WriteLine("List of allowed Users for this process: ");
+                foreach (string user in entry.Users)
+                {
+                    Console.WriteLine("\t" + user);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
             
     }
 }
