f7f98fd0f5b7111e908d8915e0d081d4ed08b335
Nikola Petric <nikola2000petric@gmail.com>
2023-01-14 13:51:40 +0100

    IDS Done

--- ProjektniZadatak16/IntrusionDetectionSystem/Program.cs
+++ ProjektniZadatak16/IntrusionDetectionSystem/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CertificateManager;
+using Contracts;
+using SecurityManager;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
@@ -14,7 +17,38 @@ namespace IntrusionDetectionSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("IDS");
+            string name = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            name = name.ToLower();
+            Console.WriteLine(name);
+
+            NetTcpBinding bindingIDS = new NetTcpBinding();
+
+            bindingIDS.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            string addressIDS = "net.tcp://localhost:9998/IDS";
+            ServiceHost host = new ServiceHost(typeof(IntrusionDetectionSystem));
+            host.AddServiceEndpoint(typeof(IIntrusionDetectionSystem), bindingIDS, addressIDS);
+
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.ChainTrust;
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, name);
+
+            try
+            {
+                host.Open();
+                Console.WriteLine("Intrusion Detection System started.");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[ERROR] {0}", e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                host.Close();
+            }
         }
     }
 }
