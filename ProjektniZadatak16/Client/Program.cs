315d0314d46a48e695d5f4333ae8c9b869fd87eb
Urostesicc <tesic.pr89.2019@uns.ac.rs>
2023-01-14 13:27:06 +0100

    program klijent

--- ProjektniZadatak16/Client/Program.cs
+++ ProjektniZadatak16/Client/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Security.Principal;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using SecurityManager;
+using Formatter = SecurityManager.Formatter;
 
 namespace Client
 {
@@ -12,7 +15,20 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Client");
+            //string name = "chiefOfficer";
+            string name = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            Console.WriteLine(name);
+
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/Service";
+
+            //Windows Authentification
+            binding.Security.Mode = SecurityMode.Transport;
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            string str;
+            bool exit = false;
         }
             
     }
